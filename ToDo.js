fa9cae96c487e08269497eb32ba3a5eb5c927032
2WDJClass7 <51160431+2WDJClass7@users.noreply.github.com>
2020-02-02 21:58:29 +0900

    style component

--- ToDo.js
+++ ToDo.js
@@ -1,14 +1,133 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions
+} from "react-native";
+
+const { width, height } = Dimensions.get("window");
 
 export default class ToDo extends Component {
+  state = {
+    isEditing: false,
+    isCompleted: false
+  };
   render() {
+    const { isCompleted, isEditing } = this.state;
     return (
-      <View>
-        <Text>Hello I'm a TO DO </Text>
+      <View style={styles.container}>
+        <View style={styles.column}>
+          <TouchableOpacity onPress={this._toggleComplete}>
+            <View
+              style={[
+                styles.circle,
+                isCompleted ? styles.completedCircle : styles.uncompletedCircle
+              ]}
+            />
+          </TouchableOpacity>
+          <Text
+            style={[
+              styles.text,
+              isCompleted ? styles.completedText : styles.uncompletedText
+            ]}
+          >
+            Hello I'm a TO DO
+          </Text>
+        </View>
+
+        {isEditing ? (
+          <View style={styles.action}>
+            <TouchableOpacity onPressOut={this._finishEditing}>
+              <View style={styles.actionContainer}>
+                <Text style={styles.actionText}>ok</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View style={styles.action}>
+            <TouchableOpacity onPressOut={this._startEditing}>
+              <View style={styles.actionContainer}>
+                <Text style={styles.actionText}>edit</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View style={styles.actionContainer}>
+                <Text style={styles.actionText}>cancle</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
     );
   }
+  _toggleComplete = () => {
+    this.setState(prevState => {
+      return {
+        isCompleted: !prevState.isCompleted
+      };
+    });
+  };
+  //편집-수정안할때 스위칭
+  _startEditing = () => {
+    this.setState({
+      isEditing: true
+    });
+  };
+  //클릭시 수정, 편집이 끝
+  _finishEditing = () => {
+    this.setState({
+      isEditing: false
+    });
+  };
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: width - 50,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "600",
+    marginVertical: 20
+  },
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderWidth: 3,
+    marginRight: 20
+  },
+  completedCircle: {
+    borderColor: "#bbb"
+  },
+  uncompletedCircle: {
+    borderColor: "#F23657"
+  },
+  completedText: {
+    color: "#bbb",
+    textDecorationLine: "line-through"
+  },
+  uncompletedText: {
+    color: "red"
+  },
+  column: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: width / 2,
+    justifyContent: "space-between"
+  },
+  action: {
+    flexDirection: "row"
+  },
+  actionContainer: {
+    marginVertical: 10,
+    marginHorizontal: 10
+  }
+});
